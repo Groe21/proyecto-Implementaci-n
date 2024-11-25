3015f27e47b81e7239eebe5383ce3035358dd773
Vlad tepes <diazcaleroluismario@gmail.com>
2024-11-25 18:15:46 -0500

    eliminar boton de los admins

--- web2/admin5.php
+++ web2/admin5.php
@@ -21,8 +21,7 @@
                 Las Aguilas del Saber
             </a>
             <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-        
+
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
